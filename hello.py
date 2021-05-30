061437fcabe9a228511112721971ca945ea1151c
EC2 Default User <ec2-user@ip-172-31-31-36.us-east-2.compute.internal>
2021-05-30 21:37:18 +0000

    Basic Login Flow

--- hello.py
+++ hello.py
@@ -1,21 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
-
+from MusicLibraryTest import login
 app = Flask(__name__)
-
 @app.route("/", methods=['GET', 'POST'])
 
 def index():
     if request.method == 'POST':
         if request.form.get('spotifylogin') == 'Login with Spotify':
-            MusicLibraryTest.login() # do something
+            login() # do something
         elif  request.form.get('search') == 'Search':
             pass # do something else
         else:
             pass # unknown
-    elif request.method == 'GET':
-        return render_template('index.html', form=form)
-    
     return render_template("index.html");
     
 if __name__ == "__main__":
