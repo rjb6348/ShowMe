f199825c25708ffaf3c007b7e3d19d35cbf16ca3
Ryley <rjb6348@psu.edu>
2021-06-06 21:08:13 -0400

    Basic Utilities for Parsing Spotify Library and Finding Artist and Events
    Still pushing to main while pre unit tests

--- MusicLibrary.py
+++ MusicLibrary.py
@@ -25,15 +25,17 @@ class SpotifyInteractions(object):
         self.sp = None 
         self.offset = 0
         self.scope = None
+        self.userName = None
 
-    def login(self):
+    def login(self, username = None):
         try:
             self.scope='user-library-read user-top-read'
-            #username = input("Type the Spotify user ID to use: ")
-            username = 'rrbvt'
+            if username == None:
+                username = input("Type the Spotify user ID to use: ")
             token = util.prompt_for_user_token(username, show_dialog=True, scope=self.scope, client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri)
             sp = spotipy.Spotify(auth = token,oauth_manager=SpotifyOAuth(scope=self.scope,client_id=self.client_id, client_secret=self.client_secret, redirect_uri=self.redirect_uri))
             self.token = token
+            self.userName = sp.me()['display_name']
             self.sp = sp
             #pprint(sp.me())
             return "Success"
@@ -51,32 +53,7 @@ class SpotifyInteractions(object):
     #TODO follow up on this function
     
     def dispUser(self):
-        print(self.sp.me())
-    
-    #def getTopTracks(self):
+        print(self.userName)
 
-    '''
-    client_id = self.client_id
-    client_secret = self.client_secret
-    redirect_uri = 'http://18.218.158.90:8080/'
-    scopes = 'user-library-read'
-    sp_oauth = oauth2.SpotifyOAuth(client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri,scope=scopes,show_dialog=True)
-    token_info = sp_oauth.get_cached_token() 
-    if not token_info:
-        auth_url = sp_oauth.get_authorize_url()
-        print(auth_url)
-        response = input('Paste the above link into your browser, then paste the redirect url here: ')
-
-        code = sp_oauth.parse_response_code(response)
-        print(code)
-        token_info = sp_oauth.get_access_token(code)
-
-    token = token_info['access_token']
-
-    sp = spotipy.Spotify(auth=token)
-
-    results = sp.current_user_saved_tracks()
-    for idx, item in enumerate(results['items']):
-        track = item['track']
-        print(idx, track['artists'][0]['name'], " – ", track['name'])
-    '''
+    def logout(self):
+        print("logout")
--- TestDriver.py
+++ TestDriver.py
@@ -1,5 +1,5 @@
 import MusicLibrary
-
+import SongKickAPI
 
 def testSpotify():
     client_id = '1b55c01382b740c0b4c6fd22f7065fd6'
@@ -8,16 +8,42 @@ def testSpotify():
 
     ml = MusicLibrary.SpotifyInteractions(client_id, client_secret, redirect_uri)
 
-    login_status = ml.login()
+    login_status = ml.login('rrbvt')
     check_status("Login", login_status)
 
     ml.dispUser()
 
     [TopArtists, TopArtist_status] = ml.pullUserTopArtists(limit=10)
     check_status("Pull Tracks", TopArtist_status)
+    print("Long_Term")
     print("Number\tArtist")
     for i, item in enumerate(TopArtists['items']):
         print (i+1,'\t',item['name']) 
+
+    [TopArtists, TopArtist_status] = ml.pullUserTopArtists(limit=10,time_range="medium_term")
+    check_status("Pull Tracks", TopArtist_status)
+    print("Medium_Term")
+    print("Number\tArtist")
+    for i, item in enumerate(TopArtists['items']):
+        print (i+1,'\t',item['name']) 
+
+    [TopArtists, TopArtist_status] = ml.pullUserTopArtists(limit=10,time_range="short_term")
+    check_status("Pull Tracks", TopArtist_status)
+    print("Short_Term")
+    print("Number\tArtist")
+    for i, item in enumerate(TopArtists['items']):
+        print (i+1,'\t',item['name']) 
+
+
+def testSongKickApi():
+    sk = SongKickAPI.SongKickAPI()
+    artistId = sk.findArtist('The Killers')
+    print(artistId)
+    LocationId = sk.findCity('Philadelphia','PA')
+    print(LocationId)
+    Events = sk.findArtistEvents(artistId)
+    print(Events)
+
 
 def check_status(step, return_status):
     if return_status.lower() == "success":
@@ -31,3 +57,4 @@ def check_status(step, return_status):
 
 if __name__ == "__main__":
     testSpotify()
+    testSongKickApi()
