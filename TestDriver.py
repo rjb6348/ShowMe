92cbe781684f0a72d3f11e78d0172a97b29b95b7
Ryley <rjb6348@psu.edu>
2021-06-06 22:17:15 -0400

    Base Unit Tests while Objects and interactions come together

--- TestDriver.py
+++ TestDriver.py
@@ -6,7 +6,7 @@ def testSpotify():
     client_secret = '0d1c6411f69342359bdaca85134120eb'
     redirect_uri = 'http://localhost'
 
-    ml = MusicLibrary.SpotifyInteractions(client_id, client_secret, redirect_uri)
+    ml = MusicLibrary.SpotifyUserInteractions(client_id, client_secret, redirect_uri)
 
     login_status = ml.login('rrbvt')
     check_status("Login", login_status)
@@ -37,13 +37,39 @@ def testSpotify():
 
 def testSongKickApi():
     sk = SongKickAPI.SongKickAPI()
-    artistId = sk.findArtist('The Killers')
+    artist = 'The Killers'
+    [status, artistId] = sk.findArtist(artist)
     print(artistId)
-    LocationId = sk.findCity('Philadelphia','PA')
+    [status, LocationId] = sk.findCity('Philadelphia','PA')
     print(LocationId)
-    Events = sk.findArtistEvents(artistId)
+    [status, Events] = sk.findArtistEvents(ArtistId = artistId, Artist = artist)
     print(Events)
 
+def testCombo():
+    client_id = '1b55c01382b740c0b4c6fd22f7065fd6'
+    client_secret = '0d1c6411f69342359bdaca85134120eb'
+    redirect_uri = 'http://localhost'
+
+    ml = MusicLibrary.SpotifyUserInteractions(client_id, client_secret, redirect_uri)
+    sk = SongKickAPI.SongKickAPI()
+
+    login_status = ml.login('rrbvt')
+    [TopArtists, TopArtist_status] = ml.pullUserTopArtists(limit=10)
+    print("Long_Term")
+    print("Number\tArtist")
+    artist_ids =[]
+    for i, item in enumerate(TopArtists['items']):
+        artist = item['name']
+        [artistSearchStatus, artistId] = sk.findArtist(artist)
+        if artistSearchStatus == "Success":
+            [status, tour] = sk.findArtistEvents(ArtistId = artistId, Artist = artist)
+            if status == "Success":
+                print(artist)
+                print("===============================")
+                for i in range(0,len(tour[0])):
+                    print(tour[0][i] + "    " + tour[1][i])
+                #print(tour)
+                print("    ")
 
 def check_status(step, return_status):
     if return_status.lower() == "success":
@@ -58,3 +84,4 @@ def check_status(step, return_status):
 if __name__ == "__main__":
     testSpotify()
     testSongKickApi()
+    testCombo()
